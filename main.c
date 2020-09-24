f151abcd71eca76796ff22231dcf38404a4f8ac3
shinyeon57 <noeys57@sookmyung.ac.kr>
2020-09-24 16:09:45 +0900

    practice 02

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int x,y;
+	
+	printf("input two integers :");
+	//scanf
+	scanf("%i %i", &x, &y);
+	
+	//calculate and printf
+	printf("+ result is : %i\n", x+y);
+	printf("- result is : %i\n", x-y);
+	printf("* result is : %i\n", x*y);
+	printf("/ result is : %i\n", x/y);
+	printf("%% result is : %i\n", x%y);
+	
+	return 0;
+}
